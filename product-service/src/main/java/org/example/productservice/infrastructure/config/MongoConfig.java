ba152b5fd21f12aec204f7b7a0c408ecb4ca6d7e
duynpce <a23.thvl03@gmail.com>
2026-08-17 16:43:57 +0700

    chore(docker): enable MongoDB transactions with replica set

--- product-service/src/main/java/org/example/productservice/infrastructure/config/MongoConfig.java
+++ product-service/src/main/java/org/example/productservice/infrastructure/config/MongoConfig.java
@@ -4,11 +4,20 @@ import org.bson.UuidRepresentation;
 import org.springframework.boot.mongodb.autoconfigure.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
+@EnableTransactionManagement
 public class MongoConfig{
     @Bean
     public MongoClientSettingsBuilderCustomizer uuidCustomizer() {
         return builder -> builder.uuidRepresentation(UuidRepresentation.STANDARD);
+    }
+
+    @Bean
+    public MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
     }
 }
